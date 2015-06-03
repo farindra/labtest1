9f1d0688e4d952295d17be1d3fe0d216398d3e2a
Farindra <farindra.project@gmail.com>
2015-06-03 14:00:50 +0700

    1st update

---
+++ advanced/common/config/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+Yii::setAlias('common', dirname(__DIR__));
+Yii::setAlias('frontend', dirname(dirname(__DIR__)) . '/frontend');
+Yii::setAlias('systemit', dirname(dirname(__DIR__)) . '/systemit');
+Yii::setAlias('lukisongroup', dirname(dirname(__DIR__)) . '/lukisongroup');
+Yii::setAlias('backend', dirname(dirname(__DIR__)) . '/backend');
+Yii::setAlias('console', dirname(dirname(__DIR__)) . '/console');
+Yii::setAlias('api', dirname(dirname(__DIR__)) . '/api'); // add api alias
