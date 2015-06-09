d029e0684462bfa6eef1fd7727290909811b9cf6
Farindra <farindra.project@gmail.com>
2015-06-09 17:46:53 +0700

    selasa sore

--- advanced/lukisongroup/web/index.php
+++ advanced/lukisongroup/web/index.php
@@ -2,6 +2,7 @@
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 defined('LUKISON_IMAGE_PATH') or define('LUKISON_IMAGE_PATH','/labtest1/advanced/lukisongroup/web/assets_sss/images/');
+defined('LUKISON_JS_PATH') or define('LUKISON_JS_PATH','/labtest1/advanced/lukisongroup/web/assets_sss/js/');
 
 require(__DIR__ . '/../../vendor/autoload.php');
 require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
